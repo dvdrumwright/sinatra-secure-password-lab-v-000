9ce632ff7f1234c004a99fabd8b55f66886ba713
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-10-10 18:15:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username]== " " && params[:password] == " "
+    if params[:username]== "" && params[:password] == ""
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
  post '/login' do
-  @user = User.find_by(params[:username])
+  @user = User.find_by(username: params[:username])
  if @user && @user.authenticate(params[:password])
   session[:user_id] = @user.id
   redirect to '/account'
